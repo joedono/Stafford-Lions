e78a3ff0dd97a02f9c6ebfdbfebafd59d93b1997
joedono <fjzusi@gmail.com>
2023-04-28 11:30:32 -0400

    Added archive of old site

---
+++ archive/Oldindex.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+<title>Stafford Lions Fall Festival</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body bgcolor="#FFFFFF" text="#000000">
+<div align="center"> 
+  <table width="91%" border="0" cellpadding="1">
+    <tr>
+      <td width="29%"><img src="images/lionhead3.jpg" width="268" height="263"></td>
+      <td width="44%"> 
+        <p align="center"><b>Stafford Lions Club</b><br>
+          <br>
+          <br>
+          Meetings: 1st &amp; 3rd Tuesdays at 6:30 PM at Bravos Restaurant,<br>
+          Stafford Springs, CT</p>
+        </td>
+      <td width="27%"><img src="images/lionlogo.gif" width="256" height="246"></td>
+    </tr>
+  </table>
+  <p>&nbsp;</p>
+  <p><b>Stafford Lions Lion Timers timed<br>
+    the &quot;Spirit of Spring&quot; Race in Tolland CT on the first Saturday 
+    of April.</b></p>
+  <p><b>Race Results <a href="spiritofspring08/OverallResults08.html">Click here</a></b></p>
+  <p>&nbsp;</p>
+  <p>&nbsp; 
+    <?php
+//echo '...';
+echo $_SERVER['SERVER_NAME'] ;
+?>
+  </p>
+  </div>
+
+</body>
+</html>
